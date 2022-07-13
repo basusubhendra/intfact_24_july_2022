96813345befbd2974f0b1b4c9e7db5e531f44223
Subhendra Basu <basu.subhendra@outlook.com>
2022-07-13 09:46:24 +0530

    Added Tuples

--- intfact.cpp
+++ intfact.cpp
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <vector>
+using namespace std;
+
+void* characterize(char* num, vector<char*>& tuples) {
+	long l = strlen(num);
+	for (long i = l - 1; i >= 0; --i) {
+		char* tuple = (char*) calloc(3, sizeof(char));
+		tuple[0] = num[l - 1 - i];
+		tuple[1] = num[i];
+		tuple[2] = '\0';
+		tuples.push_back(tuple);
+	}
+	return 0;
+}
 
 int main(int argc, char* argv[]) {
 	char* num = strdup(argv[1]);
 	printf("\nNumber entered was %s\n", num);
+	vector<char*> tuples;
+	characterize(num, tuples);
+	for(int i = 0; i < tuples.size(); ++i) {
+		printf("\n%s\n", tuples[i]);
+	}
 	return 0;
 }
