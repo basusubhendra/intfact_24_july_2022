712bf32dc54ec5d6d62649c5ce1c76caec8c17c2
Subhendra Basu <basu.subhendra@outlook.com>
2022-07-15 07:48:41 +0530

    Factorization engine in progress

--- intfact.cpp
+++ intfact.cpp
@@ -30,79 +30,189 @@ void* factorize(char* tuple, int stage, int& factor1, int& factor2, int& polarit
 	while (1) {
 		fscanf(fp, "%c", &pp);
 		fscanf(fe, "%c", &ee);
-		if (polarity == t && stage == hit && pp == tuple[0] && ee == tuple[1]) {
-			//calculate
-			int pk = pp - '0';
-			int ek = ee - '0';
-			char _pp = 0, _ee = 0;
-			fscanf(fp, "%c", &_pp);
-			fscanf(fe, "%c", &_ee);
-			int _pk = _pp - '0';
-			int _ek = _ee - '0';
-			int _last_pk = last_pp - '0';
-			int _last_ek = last_ee - '0';
-			if (pk > ek) {
-				if (_ek < _last_ek) {
-					_ek += 10;
+		if ((polarity == -1 || polarity == t) && stage == hit && pp == tuple[0] && ee == tuple[1]) {
+			if (polarity == t) {
+				polarity = t;
+				//calculate
+				int pk = pp - '0';
+				int ek = ee - '0';
+				char _pp = 0, _ee = 0;
+				fscanf(fp, "%c", &_pp);
+				fscanf(fe, "%c", &_ee);
+				int _pk = _pp - '0';
+				int _ek = _ee - '0';
+				int _last_pk = last_pp - '0';
+				int _last_ek = last_ee - '0';
+				if (pk > ek) {
+					if (_ek < _last_ek) {
+						_ek += 10;
+					}
+					if (_last_pk < _pk) {
+						_last_pk += 10;
+					}
+					factor2 = _ek - _last_ek;
+					factor1 = _last_pk - _pk;
+					if (_ek == 0 || _last_ek == 0 || _pk == 0 || _last_pk == 0) {
+						cout << "\nindeterminate at this stage\n";
+						//mark down
+					} else {
+						cout << factor1 << "\t" << factor2 << "\t" << polarity << endl;
+					}
+				} else if (pk < ek) {
+					if (_last_ek < _ek) {
+						_last_ek += 10;
+					}
+					if (_pk < _last_pk) {
+						_pk += 10;
+					}
+					factor2 = _last_ek - _ek;
+					factor1 = _pk - _last_pk;
+					if (_ek == 0 || _last_ek == 0 || _pk == 0 || _last_pk == 0) {
+						cout << "\nindeterminate at this stage\n";
+						//mark down
+					} else {
+						cout << factor1 << "\t" << factor2 << "\t" << polarity << endl;
+					}
+				} else if (pk == ek) {
+					if (_last_ek < _ek) {
+						_last_ek += 10;
+					}
+					if (_last_pk < _pk) {
+						_last_pk += 10;
+					}
+					factor2 = _last_ek - _ek;
+					factor1 = _last_pk - _pk;
+					if (_ek == 0 || _last_ek == 0 || _pk == 0 || _last_pk == 0) {
+						cout << "\nindeterminate at this stage\n";
+						//mark down
+					} else {
+						cout << factor1 << "\t" << factor2 << "\t" << polarity << endl;
+					}
 				}
-				if (_last_pk < _pk) {
-					_last_pk += 10;
+				++hit;
+				return 0;
+			} else if (polarity == -1 && t == 0) {
+				polarity = t;
+				//calculate
+				int pk = pp - '0';
+				int ek = ee - '0';
+				char _pp = 0, _ee = 0;
+				fscanf(fp, "%c", &_pp);
+				fscanf(fe, "%c", &_ee);
+				int _pk = _pp - '0';
+				int _ek = _ee - '0';
+				int _last_pk = last_pp - '0';
+				int _last_ek = last_ee - '0';
+				if (pk > ek) {
+					if (_last_ek < _ek) {
+						_last_ek += 10;
+					}
+					if (_pk < _last_pk) {
+						_pk += 10;
+					}
+					factor2 = _last_ek - _ek;
+					factor1 = _pk - _last_pk;
+					if (_ek == 0 || _last_ek == 0 || _pk == 0 || _last_pk == 0) {
+						cout << "\nindeterminate at this stage\n";
+						//mark down
+					} else {
+						cout << factor1 << "\t" << factor2 << "\t" << polarity << endl;
+					}
+				} else if (pk < ek) {
+					if (_ek < _last_ek) {
+						_ek += 10;
+					}
+					if (_last_pk < _pk) {
+						_last_pk += 10;
+					}
+					factor2 = _ek - _last_ek;
+					factor1 = _last_pk - _pk;
+					if (_ek == 0 || _last_ek == 0 || _pk == 0 || _last_pk == 0) {
+						cout << "\nindeterminate at this stage\n";
+						//mark down
+					} else {
+						cout << factor1 << "\t" << factor2 << "\t" << polarity << endl;
+					}
+				} else if (pk == ek) {
+					if (_ek < _last_ek) {
+						_ek += 10;
+					}
+					if (_pk < _last_pk) {
+						_pk += 10;
+					}
+					factor2 = _ek - _last_ek;
+					factor1 = _pk - _last_pk;
+					if (_ek == 0 || _last_ek == 0 || _pk == 0 || _last_pk == 0) {
+						cout << "\nindeterminate at this stage\n";
+						//mark down
+					} else {
+						cout << factor1 << "\t" << factor2 << "\t" << polarity << endl;
+					}
 				}
-				factor2 = _ek - _last_ek;
-				factor1 = _last_pk - _pk;
-				if (_ek == 0 || _last_ek == 0) {
-					cout << "\nfactor2 indeterminate at this stage\n";
-					cout <<  "\t" << "\nFactor1\t" << factor1 << "\t" << endl;
-				} else if (_pk == 0 || _last_pk == 0) {
-					cout << "\nfactor1 indeterminate at this stage\n";
-					cout << "\nFactor2\t" << factor2 << "\t" << endl;
-				} else {
-					cout << factor1 << "\t" << factor2 << "\t" << endl;
+				++hit;
+				return 0;
+			} else if (polarity == -1 && t == 1) {
+				polarity = t;
+				//calculate
+				int pk = pp - '0';
+				int ek = ee - '0';
+				char _pp = 0, _ee = 0;
+				fscanf(fp, "%c", &_pp);
+				fscanf(fe, "%c", &_ee);
+				int _pk = _pp - '0';
+				int _ek = _ee - '0';
+				int _last_pk = last_pp - '0';
+				int _last_ek = last_ee - '0';
+				if (pk > ek) {
+					if (_ek < _last_ek) {
+						_ek += 10;
+					}
+					if (_last_pk < _pk) {
+						_last_pk += 10;
+					}
+					factor2 = _ek - _last_ek;
+					factor1 = _last_pk - _pk;
+					if (_ek == 0 || _last_ek == 0 || _pk == 0 || _last_pk == 0) {
+						cout << "\nindeterminate at this stage\n";
+						//mark down
+					} else {
+						cout << factor1 << "\t" << factor2 << "\t" << polarity << endl;
+					}
+				} else if (pk < ek) {
+					if (_last_ek < _ek) {
+						_last_ek += 10;
+					}
+					if (_pk < _last_pk) {
+						_pk += 10;
+					}
+					factor2 = _last_ek - _ek;
+					factor1 = _pk - _last_pk;
+					if (_ek == 0 || _last_ek == 0 || _pk == 0 || _last_pk == 0) {
+						cout << "\nindeterminate at this stage\n";
+						//mark down
+					} else {
+						cout << factor1 << "\t" << factor2 << "\t" << polarity << endl;
+					}
+				} else if (pk == ek) {
+					if (_last_ek < _ek) {
+						_last_ek += 10;
+					}
+					if (_last_pk < _pk) {
+						_last_pk += 10;
+					}
+					factor2 = _last_ek - _ek;
+					factor1 = _last_pk - _pk;
+					if (_ek == 0 || _last_ek == 0 || _pk == 0 || _last_pk == 0) {
+						cout << "\nindeterminate at this stage\n";
+						//mark down
+					} else {
+						cout << factor1 << "\t" << factor2 << "\t" << polarity << endl;
+					}
 				}
-			} else if (pk < ek) {
-				if (_last_ek < _ek) {
-					_last_ek += 10;
-				}
-				if (_pk < _last_pk) {
-					_pk += 10;
-				}
-				factor2 = _last_ek - _ek;
-				factor1 = _pk - _last_pk;
-				if (_ek == 0 || _last_ek == 0) {
-					cout << "\nfactor2 indeterminate at this stage\n";
-					cout <<  "\nFactor1\t" << factor1 << "\t" << endl;
-				} else if (_pk == 0 || _last_pk == 0) {
-					cout << "\nfactor1 indeterminate at this stage\n";
-					cout << "\nFactor2\t"<< factor2 << "\t" << endl;
-				} else {
-					cout << factor1 << "\t" << factor2 << "\t" << endl;
-				}
-			} else if (pk == ek) {
-				if (_last_ek < _ek) {
-					_last_ek += 10;
-				}
-				if (_last_pk < _pk) {
-					_last_pk += 10;
-				}
-				factor2 = _last_ek - _ek;
-				factor1 = _last_pk - _pk;
-				if (_ek == 0 || _last_ek == 0) {
-					cout << "\nfactor2 indeterminate at this stage\n";
-					cout <<  "\nFactor1\t" << factor1 << "\t" << endl;
-				} else if (_pk == 0 || _last_pk == 0) {
-					cout << "\nfactor1 indeterminate at this stage\n";
-					cout << "\nFactor2\t"<< factor2 << "\t" << endl;
-				} else {
-					cout << factor1 << "\t" << factor2 << "\t" << endl;
-				}
-			}
-			++hit;
-			return 0;
-		} else if (polarity == t && stage > hit && pp == tuple[0] && ee == tuple[1]) {
-			//calculate
-			++hit;
-			return 0;
-		} else if (polarity != t && stage == hit && pp == tuple[0] && ee == tuple[1]) {
+				++hit;
+				return 0;
+			}	
+		} else if (polarity == (1 - t) && stage == hit && pp == tuple[0] && ee == tuple[1]) {
 			//mark down
 			++hit;
 		}
@@ -122,7 +232,7 @@ int main(int argc, char* argv[]) {
 	characterize(num, *tuples);
 	std::string factor_lt = "";
 	std::string factor_gt = "";
-	int polarity = 1;
+	int polarity = -1;
 	for (int i = 0; i < tuples->size(); ++i) {
 		char* tuple = tuples->at(i);
 		int factor1 = 0, factor2 = 0;
