090713c9cc3730c3db61f7017fc3fe693e00ffd7
Subhendra Basu <basu.subhendra@outlook.com>
2022-07-16 17:41:29 +0530

    Fixed Some Obvious bugs.

--- intfact.cpp
+++ intfact.cpp
@@ -22,20 +22,40 @@ void* factorize(void* arg) {
 	long numerator_sum = 0;
 	long denominator_sum = 0;
 	while (1) {
+		bool pk_hit = false, ek_hit = false;
 		fscanf(fp, "%c", &pp);
 		fscanf(fe, "%c", &ee);
-		if ((t == 1 && pp == posit2 && ee == posit1) || (t == 0 && pp == posit1 && ee == posit2)) {
+		int pk = pp - '0';
+		int ek = ee - '0';
+		bool is_pk_even = (pk % 2 == 0);
+		bool is_pk_odd = (pk % 2 == 1);
+		bool is_ek_even = (ek % 2 == 0);
+		bool is_ek_odd = (ek % 2 == 1);
+		if (t == 0 && is_pk_odd) {
+			pk_hit = true;
+		}
+		if (t == 0 && is_ek_even) {
+			ek_hit = true;
+		}
+		if (t == 1 && is_pk_even) {
+			pk_hit = true;
+		}
+		if (t == 1 && is_pk_odd) {
+			ek_hit = true;
+		}
+		if (pk_hit && ek_hit && ((t == 1 && pp == posit2 && ee == posit1) || (t == 0 && pp == posit1 && ee == posit2))) {
 			cout << "numerator_sum\t" << numerator_sum << endl;
 			cout << "denominator_sum\t" << denominator_sum << endl;
+			system("a=1; read a");
 			factor += boost::lexical_cast<std::string>(abs(numerator_sum - denominator_sum));
 			numerator_sum = 0;
 			denominator_sum = 0;
 			fscanf(f1, "%c", &posit1);
 			fscanf(f2, "%c", &posit2);
-		} else if ((t == 1 && pp == posit2) || (t == 0 && pp == posit1)) {
+		} else if (pk_hit && ((t == 1 && pp == posit2) || (t == 0 && pp == posit1))) {
 			numerator_sum += (pp - '0');
 			fscanf(f1, "%c", &posit1);
-		} else if ((t == 1 && ee == posit2) || (t == 0 && pp == posit1)) {
+		} else if (ek_hit && ((t == 1 && ee == posit2) || (t == 0 && ee == posit1))) {
 			denominator_sum += (ee - '0');
 			fscanf(f2, "%c", &posit2);
 		}
