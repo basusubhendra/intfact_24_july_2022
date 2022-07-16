0159e3dbb622e26cf88de5efaa5bb1d5ec6188fa
Subhendra Basu <basu.subhendra@outlook.com>
2022-07-16 17:28:21 +0530

    First Initial Draft of Complete Code Finished. Pre-alpha. Testing Begins.

--- intfact.cpp
+++ intfact.cpp
@@ -1,31 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <string>
 #include <iostream>
+#include <boost/lexical_cast.hpp>
+using namespace std;
+using namespace boost;
 
 void* factorize(void* arg) {
-	struct return_packet* ret = (struct return_packet*) calloc(1, sizeof(struct return_packet));
+	int t = *(int*) arg;
+	std::string factor = "";
 	FILE* f1 = fopen64("./op.txt", "r");
 	FILE* f2 = fopen64("./op2.txt", "r");
-	fseek(f1, p1, SEEK_SET);
-	fseek(f2, p2, SEEK_SET);
+	FILE* fp = fopen64("./pi.txt", "r");
+	FILE* fe = fopen64("./e.txt", "r");
 	char posit1 = 0, posit2 = 0;
+	char pp = 0, ee = 0;
+	fscanf(f1, "%c", &posit1);
+	fscanf(f2, "%c", &posit2);
+	long numerator_sum = 0;
+	long denominator_sum = 0;
 	while (1) {
-		fscanf(f1, "%c", &pp);
-		fscanf(f2, "%c", &ee);
-		if (pp == 
+		fscanf(fp, "%c", &pp);
+		fscanf(fe, "%c", &ee);
+		if ((t == 1 && pp == posit2 && ee == posit1) || (t == 0 && pp == posit1 && ee == posit2)) {
+			cout << "numerator_sum\t" << numerator_sum << endl;
+			cout << "denominator_sum\t" << denominator_sum << endl;
+			factor += boost::lexical_cast<std::string>(numerator_sum - denominator_sum);
+			numerator_sum = 0;
+			denominator_sum = 0;
+			fscanf(f1, "%c", &posit1);
+			fscanf(f2, "%c", &posit2);
+		} else if ((t == 1 && pp == posit2) || (t == 0 && pp == posit1)) {
+			numerator_sum += (pp - '0');
+			fscanf(f1, "%c", &posit1);
+		} else if ((t == 1 && ee == posit2) || (t == 0 && pp == posit1)) {
+			denominator_sum += (ee - '0');
+			fscanf(f2, "%c", &posit2);
+		}
 	}
 	fclose(f1);
 	fclose(f2);
-	return ret;
+	fclose(fp);
+	fclose(fe);
+	return strdup((char*) factor.c_str());
 }
 
 int main() {
 	char* ret1, *ret2;
+	int t = 0;
 	pthread_t thread_id1, thread_id2;
-	pthread_create(&thread_id1, NULL, factorize, NULL);
-	pthread_create(&thread_id2, NULL, factorize, NULL);
+	pthread_create(&thread_id1, NULL, factorize, &t);
+	t = 1 - t;
+	pthread_create(&thread_id2, NULL, factorize, &t);
 	pthread_join(thread_id1, (void**) &ret1);
 	pthread_join(thread_id2, (void**) &ret2);
 	return 0;
