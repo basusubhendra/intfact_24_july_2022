bdc6df3642f41c753f48d0690af10fa1d88f14e5
Subhendra Basu <basu.subhendra@outlook.com>
2022-07-23 09:59:57 +0000

    code

--- intfact.cpp
+++ intfact.cpp
@@ -6,6 +6,22 @@
 #include <iostream>
 #include <gmp.h>
 using namespace std;
+#define NZEROS 100000
+
+int binarySearch(int arr, int x, int low, int high)
+	if (low > high) {
+		return -1;
+	} else {
+		mid = (low + high) / 2; 
+		if (x == arr[mid]) {
+			return mid;
+		} else if (x > arr[mid]) {
+			return binarySearch(arr, x, mid + 1, high);
+		} else {
+			return binarySearch(arr, x, low, mid - 1) 
+		}
+	}
+}
 
 int main() {
 	return 0;
