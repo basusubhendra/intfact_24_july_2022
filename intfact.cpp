6703d4f60232bfd6020ab7287f85e8eb7b037dd8
Subhendra Basu <basu.subhendra@outlook.com>
2022-07-23 11:01:41 +0000

    code

--- intfact.cpp
+++ intfact.cpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <iostream>
 #include <gmp.h>
+#include <boost/lexical_cast.hpp>
+using namespace boost;
 using namespace std;
 #define NZEROS 100000
 
@@ -26,7 +28,7 @@ int binarySearch(int arr, int x, int low, int high)
 void* strrev(char* ee) {
 	long l = strlen(ee);
 	for (int i = 0; i < int(l / 2); ++i) {
-	        char t = ee[i];
+		char t = ee[i];
 		ee[i] = ee[l - 1 - i];
 		ee[l - 1 - i] = t;
 	}
@@ -39,6 +41,10 @@ int main(int argc, char* argv[]) {
 	long l = strlen(num);
 	long ncycles = ceil(l/3)*4;
 	long j = ncycles;
+	int hit = 0, t = 0;
+	std::string _factor_odd = "", _factor_even = "";
+	int ctr = 0;
+	vector<int> int_params;
 	while (1) {
 		long nlength = j*l;
 		char* pp = (char*) calloc(nlength + 1, sizeof(char));
@@ -50,8 +56,43 @@ int main(int argc, char* argv[]) {
 		ee[nlength] = '\0';
 		for (int i = 0; i < nlength; ++i) {
 			char nn = num[i % l];
+			char _pp = pp[i];
+			char _ee = ee[i];
+			char _payload[3];
+			_payload[0] = _pp;
+			_payload[1] = _ee;
+			_payload[2] = '\0';
+			char _nn[3];
+			_nn[0] = nn[0];
+			_nn[2] = '\0';
+			for (char k = '0'; k < '9'; ++k) {
+				_nn[1] = k;
+				int nk = atoi(_nn);
+				int _payload_k = atoi(_payload);
+				if (_payload_k == 0) _payload_k = 100;
+				if (nk > 0) {
+					int _payload_k_rem = _payload_k % nk;
+					if (_payload_k_rem == 0) {
+						int_params.push_back(k - '0');
+						params += _payload_k / nk;
+						++hit;
+						if (hit % 5 == 0) {
+							if (params % 10 == 0 || binarySearch(zeros, params, 0, NZEROS) != -1) {
+								long snippet = _calculate(ctr, int_params);
+								if (t == 0) {
+									_factor_odd += boost::lexical_cast<std::string>(snippet);
+								} else if (t == 1) {
+									_factor_even += boost::lexical_cast<std::string>(snippet);
+								}
+								t = 1 - t;
+							}
+						}	
+					}
+				}
+			}
 		}
 		--j;
+		++ctr;
 	}
 	return 0;
 }
