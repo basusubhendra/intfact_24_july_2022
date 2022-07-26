3c7a6550e2ece2964a858526ca9f283e59f0aa9b
Subhendra Basu <basu.subhendra@outlook.com>
2022-07-26 07:27:35 +0530

    code

--- synthesis.py
+++ synthesis.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 import sys
+from mpmath import *
 
 f=open("./zero_indices.txt","r")
 c=f.readlines()
 f.close()
 waveform = []
+mp.prec = 256
+mp.dps = 256
 for line in c:
     l = str(line.rstrip())
     waveform.append(l)
@@ -14,4 +17,7 @@ l = len(waveform)
 for k in range(0, int(l / 2)):
     wave1=waveform[k]
     wave2=waveform[-(k+1)]
-    print(wave1, wave2)
+    z1 = str(zetazero(wave1).imag)
+    z2 = str(zetazero(wave2).imag)
+    print(z1, z2)
+
