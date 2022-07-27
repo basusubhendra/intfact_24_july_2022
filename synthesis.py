6e1475bf5a3f08b8f55487deddc9677e88ef0e96
Subhendra Basu <basu.subhendra@outlook.com>
2022-07-27 10:50:25 +0530

    Middle of Synthesis

--- synthesis.py
+++ synthesis.py
@@ -5,7 +5,7 @@ from mpmath import *
 
 #synthesize num 
 # from z1 using z2
-def synthesize(z1, z2, num):
+def synthesize(z1, z2, n1, n2, rn1, rn2):
     pass
 
 num = str(argv[1])
@@ -17,22 +17,26 @@ f=open("./zero_indices.txt","r")
 c=f.readlines()
 f.close()
 waveform = []
-mp.prec = 85
-mp.dps = 85
+mp.prec = 256
+mp.dps = 256
 _factor1 = ""
 _factor2 = ""
 for line in c:
-    l = str(line.rstrip())
-    waveform.append(l)
-l = len(waveform)
-for k in range(0, int(l / 2)):
+    _l = str(line.rstrip())
+    waveform.append(_l)
+_l = len(waveform)
+for k in range(0, int(_l / 2)):
     wave1=waveform[k]
     wave2=waveform[-(k+1)]
     z1 = str(zetazero(wave1).imag)
     z2 = str(zetazero(wave2).imag)
-    print(z1, z2)
-    factor1 = synthesize(z1, z2, num)
-    factor2 = synthesize(z2, z1, num[::-1])
+    z1.replace(".","0")
+    z2.replace(".","0")
+    n1 = num[i]
+    n2 = num[i + 1]
+    rn1 = rnum[l - i - 1]
+    rn2 = rnum[l - i - 2]
+    factor1, factor2 = synthesize(z1, z2, n1, n2, rn1, rn2)
     _factor1 = _factor1 + factor1
     _factor2 = _factor2 + factor2
 _factor2 = _factor2[::-1]
