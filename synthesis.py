d04dc50f7ae446c30cf069ed647e70d04ccf0951
Subhendra Basu <basu.subhendra@outlook.com>
2022-07-27 11:01:10 +0530

    Dry Run started

--- synthesis.py
+++ synthesis.py
@@ -6,13 +6,22 @@ from mpmath import *
 #synthesize num 
 # from z1 using z2
 def synthesize(z1, z2, n1, n2, rn1, rn2):
-    print(n1, n2)
-    print(rn1, rn2)
-    print(z1)
-    print(z2)
-    print
-    print
-    input("Enter")
+    if t == 0:
+        print(n1, n2)
+        print(rn2, rn1)
+        print(z1)
+        print(z2)
+        print
+        print
+        input("Enter")
+    else t == 1:
+        print(n2, n1)
+        print(rn1, rn2)
+        print(z1)
+        print(z2)
+        print
+        print
+        input("Enter")
 
 num = str(sys.argv[1])
 rnum = num[::-1]
@@ -32,6 +41,7 @@ for line in c:
     waveform.append(_l)
 _l = len(waveform)
 i = 0
+t = 0
 for k in range(0, int(_l / 2)):
     wave1=waveform[k]
     wave2=waveform[-(k+1)]
@@ -43,7 +53,8 @@ for k in range(0, int(_l / 2)):
     n2 = num[i + 1]
     rn1 = rnum[i]
     rn2 = rnum[i + 1]
-    factor1, factor2 = synthesize(z1, z2, n1, n2, rn1, rn2)
+    factor1, factor2 = synthesize(z1, z2, n1, n2, rn1, rn2, t)
+    t = 1 - t
     _factor1 = _factor1 + factor1
     _factor2 = _factor2 + factor2
     i = i + 1
