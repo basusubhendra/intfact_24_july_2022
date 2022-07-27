0ac65a22b6fd13bec4be1b96c1df16a50215d782
Subhendra Basu <basu.subhendra@outlook.com>
2022-07-27 08:05:23 +0530

    Starting Synthesis Phase of Algorithm Intfact, Coming out of the Chakravyuha

--- synthesis.py
+++ synthesis.py
@@ -3,12 +3,20 @@
 import sys
 from mpmath import *
 
+#synthesize num 
+# from z1 using z2
+def synthesize(z1, z2, num):
+    pass
+
+num = str(argv[1])
 f=open("./zero_indices.txt","r")
 c=f.readlines()
 f.close()
 waveform = []
-mp.prec = 64
-mp.dps = 64
+mp.prec = 85
+mp.dps = 85
+_factor1 = ""
+_factor2 = ""
 for line in c:
     l = str(line.rstrip())
     waveform.append(l)
@@ -19,3 +27,10 @@ for k in range(0, int(l / 2)):
     z1 = str(zetazero(wave1).imag)
     z2 = str(zetazero(wave2).imag)
     print(z1, z2)
+    factor1 = synthesize(z1, z2, num)
+    factor2 = synthesize(z2, z1, num[::-1])
+    _factor1 = _factor1 + factor1
+    _factor2 = _factor2 + factor2
+_factor2 = _factor2[::-1]
+print(_factor1)
+print(_factor2)
